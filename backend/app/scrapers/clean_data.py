9c369e5b080e7408aa32420bfd1465d8f41da24d
sagar prakash kawad <sagzone@sagars-MacBook-Air.local>
2025-03-04 16:29:10 +0530

    summarization work

--- backend/app/scrapers/clean_data.py
+++ backend/app/scrapers/clean_data.py
@@ -21,4 +21,6 @@ def clean_scraped_data(html_content):
     clean_text = extract(html_content, include_links=False,
                          include_tables=False, include_images=False)
 
+    # Remove newline characters (\n)
+    clean_text = clean_text.replace("\n", " ")
     return clean_text
